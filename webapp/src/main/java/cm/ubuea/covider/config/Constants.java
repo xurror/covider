1809b20642c46df00816c63018dcb91cbb78e2a9
xurror <kaze.nasser@outlook.com>
2020-06-20 15:48:55 +0100

    Setup server configs

---
+++ webapp/src/main/java/cm/ubuea/covider/config/Constants.java
@@ -0,0 +1,14 @@
+package cm.ubuea.covider.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+    public static final String DEFAULT_LANGUAGE = "en";
+    public static final String ANONYMOUS_USER = "anonymoususer";
+
+    private Constants() {
+    }
+}
