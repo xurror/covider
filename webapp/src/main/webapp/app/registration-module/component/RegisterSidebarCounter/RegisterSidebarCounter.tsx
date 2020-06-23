6194a262a31b4163285f88036ba33c2c05788966
usmailaabdoul <ismaelabdul77@gmail.com>
2020-06-23 22:45:26 +0100

    skeleton for the structure of the registration module

---
+++ webapp/src/main/webapp/app/registration-module/component/RegisterSidebarCounter/RegisterSidebarCounter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaCheckCircle } from "react-icons/fa";
+
+import './SidebarCounter.css';
+
+const RegisterSidebarCounter = (props) => {
+  return (
+    <div>
+      RegisterSidebarCounter
+    </div>
+  )
+}
+
+export default RegisterSidebarCounter;
---
+++ webapp/src/main/webapp/app/registration-module/location/UserLocation.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { FaArrowLeft } from "react-icons/fa";
+import { Form, } from 'react-bootstrap';
+import './UserLocation.css';
+
+class UserLocation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentLocation: '',
+      previousLocation: '',
+    }
+  }
+
+  currentLocation = (event) => {
+    this.setState({ currentLocation: event.target.value })
+  }
+  previousLocation = (event) => {
+    this.setState({ previousLocation: event.target.value })
+  }
+
+
+
+  render() {
+    const { changeStage } = this.props;
+    return (
+      <div className="wrapper shadow">
+        location 
+      </div>
+    )
+  }
+}
+
+export default UserLocation;
