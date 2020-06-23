ab75eb75006e15833a01656b61e2ab11ff741b2e
usmailaabdoul <ismaelabdul77@gmail.com>
2020-06-23 22:45:26 +0100

    skeleton for the structure of the registration module

--- webapp/src/main/java/cm/ubuea/covider/ServerApplication.java
+++ webapp/src/main/java/cm/ubuea/covider/ServerApplication.java
@@ -35,13 +35,6 @@ public class ServerApplication {
         return builder.sources(Configuration.class);
     }
 
-    /**
-     * Initializes covider.
-     * <p>
-     * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
-     * <p>
-     * You can find more information on how profiles work with JHipster on <a href="https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
-     */
     @PostConstruct
     public void initApplication() {
         Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
