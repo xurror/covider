596460f05a22df788e79756e1ac12d9ee6fb7449
usmailaabdoul <ismaelabdul77@gmail.com>
2020-06-23 22:45:26 +0100

    skeleton for the structure of the registration module

---
+++ webapp/src/main/webapp/app/registration-module/loginSuccesModal/LoginSuccesModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Modal, Spinner } from 'react-bootstrap';
+
+function LoginSuccesModal(props) {
+  return (
+    <div>
+      login success Modal
+    </div>>
+  );
+}
+
+export default LoginSuccesModal;
