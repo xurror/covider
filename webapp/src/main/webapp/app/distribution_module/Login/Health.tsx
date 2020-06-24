7c3a35fecb21c6e8b2355a02e5b72762adeb165c
Ifeanyijasper <ifeanyijasper11@gmail.com>
2020-06-24 07:44:30 +0100

    Adding webapp frontend

---
+++ webapp/src/main/webapp/app/distribution_module/Login/Health.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import  './Health.css'
+
+function Health(){
+    return(
+        <div>
+            <h1>
+                Hello 
+            </h1>
+        </div>
+
+    )
+
+}
+export default Health
