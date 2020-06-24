443b0724486e497ff8519f53b85d8d0779b485e2
HarveyNgoe <harveykolle@gmail.com>
2020-06-24 15:43:56 +0100

    Splash, login, signup and forgot password screens design

---
+++ mobile-client/app/src/main/java/com/example/cef440/PersonalInformation.java
@@ -0,0 +1,14 @@
+package com.example.cef440;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_information);
+    }
+}
