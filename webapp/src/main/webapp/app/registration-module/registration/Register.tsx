db3c653e7d05b347494c217a405d8744c95a85b1
RuthAtem <mbinoatem@gmail.com>
2020-06-24 10:55:31 +0100

    Registration screen

--- webapp/src/main/webapp/app/registration-module/registration/Register.tsx
+++ webapp/src/main/webapp/app/registration-module/registration/Register.tsx
@@ -48,7 +48,60 @@ export default Register;
 const RenderRegisterHome = (props) => {
   return (
     <div>
-     Registration
+      <div className="wrapper shadow ct">
+        <div className="d-flex justify-content-between">
+          <div className="p-2">
+            <h4>Register</h4>
+          </div>
+          <div className="p-2">
+            <p className='link dim underline pointer'
+              style={{ color: "#B8C5D9" }}>
+              Having trouble? <strong className="helpColor">Get Help</strong></p>
+          </div>
+        </div>
+
+        <div style={{ marginTop: "2rem", }}>
+          <p style={{ fontSize: "1.2rem", }}>Choose your member role</p>
+          <p style={{ fontSize: ".8rem" }}>Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae</p>
+        </div>
+
+        <div>
+          <div onClick={() => {
+            const stage = {
+              stage: 2,
+              role: 'agent',
+            };
+            props.changeStage(stage)
+          }}
+            className=' dib br2 grow bw2 shadow-3 d-flex flex-row align-items-center choicecards'
+            style={{ color: "#000000", textDecoration: "none", marginTop: 40 }}>
+            <div className="iconStyle ">
+              <h3 className="icon" style={{ color: "#000000a4", fontSize: "2rem", left: "1px" }}><FaUserSecret /></h3>
+            </div>
+            <div style={{ paddingLeft: "1rem", marginBottom: -5 }}>
+              <p style={{ marginBottom: -2 }}>Agent</p>
+              <p>Lorem ipsum</p>
+            </div>
+          </div>
+
+          <div onClick={() => {
+            const stage = {
+              stage: 2,
+              role: 'user',
+            };
+            props.changeStage(stage)
+          }} className=' dib br2 grow bw2 shadow-3 d-flex flex-row align-items-center choicecards'
+            style={{ color: "#000000", textDecoration: "none" }}>
+            <div className="iconStyle ">
+              <h3 className="icon" style={{ color: "#000000a4", fontSize: "2rem", left: "1px" }}><FaUser /></h3>
+            </div>
+            <div style={{ paddingLeft: "1rem", marginBottom: -5 }}>
+              <p style={{ marginBottom: -2 }}>User</p>
+              <p>Lorem ipsum</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
