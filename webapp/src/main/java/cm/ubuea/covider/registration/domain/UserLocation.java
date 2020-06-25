b8d30983fdc62c9e8b95c5b059780ed599c1b713
lobeserge <lobeserge100@gmail.com>
2020-06-25 20:11:29 +0100

    added functionality to add user's visited locations and medical status

--- webapp/src/main/java/cm/ubuea/covider/registration/domain/UserLocation.java
+++ webapp/src/main/java/cm/ubuea/covider/registration/domain/UserLocation.java
@@ -2,7 +2,11 @@ package cm.ubuea.covider.registration.domain;
 
 
 import javax.persistence.*;
+<<<<<<< HEAD
 import javax.validation.constraints.NotNull;
+=======
+import javax.validation.constraints.NotBlank;
+>>>>>>> added functionality to add user's visited locations and medical status
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -11,6 +15,7 @@ import java.util.List;
 public class UserLocation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+<<<<<<< HEAD
     private Long id;
 
     @NotNull
@@ -26,11 +31,29 @@ public class UserLocation {
     @JoinTable(name = "c_user_location",
         joinColumns = {@JoinColumn(name = "location_id", referencedColumnName = "id")},
         inverseJoinColumns = {@JoinColumn(name = "user_id_number", referencedColumnName = "id_number")})
+=======
+    private Long location_id;
+
+    @NotBlank
+    @Size(max = 20)
+    private String current_loctaion;
+
+    @ElementCollection
+    @CollectionTable(name = "previous_user_location", joinColumns = @JoinColumn(name = "location_id"))
+    @Column(name = "previous_location")
+    private List<String > previous_location;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinTable(name = "c_user_location",
+        joinColumns = {@JoinColumn(name = "location_id", referencedColumnName = "location_id")},
+        inverseJoinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")})
+>>>>>>> added functionality to add user's visited locations and medical status
     private User user;
 
     public UserLocation() {
     }
 
+<<<<<<< HEAD
     public Long getLocationId() {
         return id;
     }
@@ -53,6 +76,30 @@ public class UserLocation {
 
     public void setPreviousLocation(List<String> previousLocation) {
         this.previousLocation = previousLocation;
+=======
+    public Long getLocation_id() {
+        return location_id;
+    }
+
+    public void setLocation_id(Long location_id) {
+        this.location_id = location_id;
+    }
+
+    public String getCurrent_loctaion() {
+        return current_loctaion;
+    }
+
+    public void setCurrent_loctaion(String current_loctaion) {
+        this.current_loctaion = current_loctaion;
+    }
+
+    public List<String> getPrevious_location() {
+        return previous_location;
+    }
+
+    public void setPrevious_location(List<String> previous_location) {
+        this.previous_location = previous_location;
+>>>>>>> added functionality to add user's visited locations and medical status
     }
 
     public User getUser() {
