9fa56427d044ee5f48262e0cc1e23dd5eeb905cc
lobeserge <lobeserge100@gmail.com>
2020-06-25 20:11:29 +0100

    added functionality to add user's visited locations and medical status

--- webapp/src/main/java/cm/ubuea/covider/registration/repository/UserLocationRepository.java
+++ webapp/src/main/java/cm/ubuea/covider/registration/repository/UserLocationRepository.java
@@ -4,10 +4,16 @@ import cm.ubuea.covider.registration.domain.UserLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+<<<<<<< HEAD
 import java.util.Optional;
 
 
 @Repository
 public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
     Optional<UserLocation> findByUser_IdNumber(String id);
+=======
+
+@Repository
+public interface UserLocationRepository extends JpaRepository<UserLocation, Long> {
+>>>>>>> ed505e9... added functionality to add user's visited locations and medical status
 }
