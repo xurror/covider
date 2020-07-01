edbe49fb7c21ca8cba6316b12ea4dc16315d7310
Jessie <djuatiojessie@gmail.com>
2020-07-01 16:39:36 +0100

    Added one page

--- mobile-client/app/src/main/java/cm/ubuea/covider/dmList.java
+++ mobile-client/app/src/main/java/cm/ubuea/covider/dmList.java
@@ -2,13 +2,11 @@ package cm.ubuea.covider;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 
 public class dmList extends AppCompatActivity {
