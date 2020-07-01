4e909e1348d6d8c5ec1b34373881fdb8fff87063
Durrell Gemuh <durrellgemuh07@gmail.com>
2020-07-02 00:04:05 +0100

    solving merge conflicts

--- mobile-client/app/src/main/java/cm/ubuea/covider/dmDisTrack.java
+++ mobile-client/app/src/main/java/cm/ubuea/covider/dmDisTrack.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
--- mobile-client/app/src/main/java/cm/ubuea/covider/dmUserLocation.java
+++ mobile-client/app/src/main/java/cm/ubuea/covider/dmUserLocation.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
