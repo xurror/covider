92cd5423abe0ece72cc28274f3b36790b28edd5e
HarveyNgoe <harveykolle@gmail.com>
2020-07-01 13:45:01 +0100

    Update to splash, login, signup and design of user home page

--- mobile-client/app/src/main/java/cm/ubuea/covider/MainActivity.java
+++ mobile-client/app/src/main/java/cm/ubuea/covider/MainActivity.java
@@ -1,5 +1,7 @@
 package cm.ubuea.covider;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,48 +10,35 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    ProgressDialog pDialog;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run(){
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+
+                pDialog = new ProgressDialog(MainActivity.this);
+                pDialog.setCancelable(false);
+                pDialog.setIndeterminate(false);
+                pDialog.setMessage("Loading...");
+                pDialog.show();
             }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        }, SPLASH_DISPLAY_LENGTH);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
