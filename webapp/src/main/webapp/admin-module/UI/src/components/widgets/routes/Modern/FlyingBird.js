61d5082321391cedb7e81e52e15844fc6e34c3ef
Fogha <collinslord50@gmail.com>
2020-07-03 12:39:34 +0100

    create file for the UI and added basic settings

---
+++ webapp/src/main/webapp/admin-module/UI/src/components/widgets/routes/Modern/FlyingBird.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+
+import Widget from "components/Widget/index";
+
+
+const FlyingBird = () => {
+  return (
+    <Widget>
+      <div className="media align-items-center mb-4">
+        <div className="mr-3">
+          <img src={"https://via.placeholder.com/60x60"} alt="flying"/>
+        </div>
+        <div className="media-body">
+          <h2 className="mb-0">
+            Flying bird
+          </h2>
+          <p className="text-grey mb-0">Bob Bush</p>
+        </div>
+      </div>
+      <p className="mb-4">Some description about the card. This widget
+        could be used to describe a project, a product,
+        user’s profile or may be more.</p>
+      <form className="mt-lg-3">
+        <Button variant="contained" color="primary" className="mb-0 text-capitalize" htmltype="submit">
+          Subscribe
+        </Button>
+      </form>
+    </Widget>
+  );
+};
+
+export default FlyingBird;
