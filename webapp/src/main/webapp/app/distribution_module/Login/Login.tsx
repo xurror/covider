feda19c61b1ecc3c94c883764e18db36c6ff6b55
Ifeanyijasper <ifeanyijasper11@gmail.com>
2020-07-03 20:11:50 +0100

    add the router

--- webapp/src/main/webapp/app/distribution_module/Login/Login.tsx
+++ webapp/src/main/webapp/app/distribution_module/Login/Login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import  './Login.css'
-import Health from './Health'
+
 import logo from './user.png'
 
 function Login(){
@@ -20,12 +20,12 @@ function Login(){
                 <div className="textbox">
                     <input type="password" placeholder="ID Card number" id=""/>
                 </div>
-                <input type="button" className="button" onClick={() => <Health/>} value="Verify"/>
+                <input type="button" className="button" value="Verify"/>
                 <div className="checkbox">
                     <input type="checkbox" id="cbremember"/>
                     <label form="chremember">Remember Me</label>
                     <a href="text.html">Forgot Password?</a>
-                </div>Health
+                </div>
             </div>
             <div className="signup-box">
                 Not A Member? <a href="text.html">Register</a>
