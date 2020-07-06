28bbf8abf460d732d61eea0a22006a92ef096a86
Tisighe Livinstone <tisighelivinstone1b@gmail.com>
2020-07-06 10:33:24 +0100

    SignIn Form

--- webapp/src/main/webapp/app.js
+++ webapp/src/main/webapp/app.js
@@ -1,14 +1,48 @@
 import React from "react"
 import useSticky from "./hooks/useSticky.js"
 import Navbar from "./components/Header/Navbar"
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import Login from "./components/SignIn";
 function App() {
   const { isSticky, element } = useSticky()
   return (
-    <>
+    <Route>
       <Navbar sticky={isSticky} />
       <Welcome element={element} />
-    </>
+
+    <div className="App">
+     
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>Group 9</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div>
+
+    </Route>
   )
 }
 
