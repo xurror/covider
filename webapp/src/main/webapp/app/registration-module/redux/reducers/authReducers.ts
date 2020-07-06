8094ff9e275b216b867037e68f39d53e11c51ed3
usmailaabdoul <ismaelabdul77@gmail.com>
2020-07-06 06:43:50 +0100

    adding remaining pages and setting up redux

---
+++ webapp/src/main/webapp/app/registration-module/redux/reducers/authReducers.ts
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  token: {},
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'SET_TOKEN':
+      return {...state, token: action.payload};
+    default:
+      return state;
+  }
+};
