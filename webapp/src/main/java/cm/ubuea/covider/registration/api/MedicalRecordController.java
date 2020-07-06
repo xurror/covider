99b18f74fd7559acfe54478bb62617cf15a0d290
lobeserge <lobeserge100@gmail.com>
2020-07-06 15:41:14 +0100

    updated medical record controller

--- webapp/src/main/java/cm/ubuea/covider/registration/api/MedicalRecordController.java
+++ webapp/src/main/java/cm/ubuea/covider/registration/api/MedicalRecordController.java
@@ -1,25 +1,34 @@
 package cm.ubuea.covider.registration.api;
 
+import cm.ubuea.covider.registration.domain.MedicalRecord;
+import cm.ubuea.covider.registration.repository.MedicalRecordRepository;
+import cm.ubuea.covider.registration.repository.UserRepository;
 import cm.ubuea.covider.registration.service.MedicalRecordService;
 import cm.ubuea.covider.registration.service.dto.MedicalRecordDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/medical")
 public class MedicalRecordController {
     final
     MedicalRecordService medicalRecordService;
+    final
+    MedicalRecordRepository medicalRecordRepository;
 
-    public MedicalRecordController(MedicalRecordService medicalRecordService) {
+    final
+    UserRepository userRepository;
+
+    public MedicalRecordController(MedicalRecordService medicalRecordService, MedicalRecordRepository medicalRecordRepository, UserRepository userRepository) {
         this.medicalRecordService = medicalRecordService;
+        this.medicalRecordRepository = medicalRecordRepository;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/")
@@ -27,4 +36,41 @@ public class MedicalRecordController {
         medicalRecordService.addMedicalRecord(medicalRecordDTO);
         return new ResponseEntity("Medical Record successfully added",HttpStatus.CREATED);
     }
+
+    @DeleteMapping("/delete/{id}")
+    void deleteMedicalREcord(@PathVariable("id") Long id) {
+        medicalRecordRepository.deleteById(id);
+    }
+
+    @GetMapping("/record/{uidcard}")
+    public List<MedicalRecord> getUserMedicalRecord(@PathVariable("uidcard") String uidcard) {
+
+        List<MedicalRecord> medic=medicalRecordRepository.findAll().
+            stream().
+            filter(e->e.getUser().getIdNumber().equals(uidcard)).
+            collect(Collectors.toList());
+
+        return medic;
+    }
+
+    @PutMapping("/update/{id}")
+    MedicalRecord updateMedicalRecord(@RequestBody MedicalRecordDTO medicalRecordDTO, @PathVariable("id") long id) {
+
+        return medicalRecordRepository.findById(id)
+            .map(m -> {
+                m.setCurrent_status(medicalRecordDTO.isCurrent_status());
+                m.setCurrent_symptoms(medicalRecordDTO.getCurrent_symptoms());
+                m.setUser(userRepository.findOneByIdNumber(medicalRecordDTO.getUserIdCard()).get());
+                return medicalRecordRepository.save(m);
+            })
+            .orElseGet(() -> {
+               return null;
+
+            });
+    }
+
+
+
+
+
 }
