4973327623bba20e92b686cf41504552b59c06cd
Ifeanyijasper <ifeanyijasper11@gmail.com>
2020-07-06 17:09:56 +0100

    Add navigation file and error file

---
+++ webapp/src/main/webapp/app/distribution_module/Geolocation/Geolocaton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const Geolocation = () => {
+    return (
+       <div>
+          <h1>User Location</h1>
+          <p>Location of users on google map body content</p>
+       </div>
+    );
+}
+ 
+export default Geolocation;
