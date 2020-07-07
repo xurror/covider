f2d5fee8597d4bed392d6cc536918b0d7030d206
DBryzz <domoubrice@gmail.com>
2020-07-07 14:23:20 +0100

    added swagger for documentation

---
+++ cef440.admin.module/src/main/java/com/admin/module/controller/LocationController.java
@@ -0,0 +1,17 @@
+/*
+ * package com.admin.module.controller;
+ * 
+ * import org.springframework.web.bind.annotation.CrossOrigin; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * @RestController
+ * 
+ * @CrossOrigin
+ * 
+ * @RequestMapping("/api") public class LocationController {
+ * 
+ * private Location userLocation
+ * 
+ * }
+ */
