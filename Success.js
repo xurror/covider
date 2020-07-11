fb80bea2f1cbd415fe7b5271cb4a30b9aeb766cd
Jenniline Ebai <39647995+Jenniline@users.noreply.github.com>
2020-07-11 21:41:50 +0100

    React files for Profile Module
    React files for Profile Module

---
+++ Success.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+export class Success extends Component {
+    render() {
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Confirmation"/>
+                    
+                   <h1>Thanks for your cooperation you can Sign Out 
+                       <br /> 
+                       If you wish
+                   </h1>
+
+                        <br/>
+
+                    <RaisedButton
+                        label="Sign Out"
+                        primary={true}
+                        style={styles.button}
+                        onClick={this.continue} //what does continue do? It calls nextStep
+                    />
+
+                    <RaisedButton
+                        label="Back"
+                        primary={true}
+                        style={styles.button}
+                        onClick={this.back} //what does continue do? It calls nextStep
+                    />
+
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+const styles = {
+    button:{
+        margin:15
+    }
+}
+
+export default Success
