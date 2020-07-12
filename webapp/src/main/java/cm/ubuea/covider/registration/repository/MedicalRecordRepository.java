760a033100dd0bbcc3265c0496bac3634319adbb
xurror <kaze.nasser@outlook.com>
2020-07-12 05:56:00 +0100

    fix merge conflicts in repositories

--- webapp/src/main/java/cm/ubuea/covider/registration/repository/MedicalRecordRepository.java
+++ webapp/src/main/java/cm/ubuea/covider/registration/repository/MedicalRecordRepository.java
@@ -4,14 +4,9 @@ import cm.ubuea.covider.registration.domain.MedicalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import java.util.Optional;
 
 @Repository
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord,Long> {
     Optional<MedicalRecord> findByUser_idNumber(String userIdNumber);
-=======
-@Repository
-public interface MedicalRecordRepository extends JpaRepository<MedicalRecord,Long> {
->>>>>>> ed505e9... added functionality to add user's visited locations and medical status
 }
